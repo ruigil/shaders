8957bb7c2f8def0e8bfed77da2b94f2c42284ec3
Rui Gil <rui@gil.com>
2020-04-14 08:48:06 +0000

    added time movement

--- sketch/maze.glsl
+++ sketch/maze.glsl
@@ -2,7 +2,7 @@
 #define ref ((gl_FragCoord.xy - iResolution.xy *.5) / ( iResolution.x < iResolution.y ? iResolution.x : iResolution.y) * 2.) 
 
 #include '../utils/2d-utils.glsl'
-#include '../utils/hash.glsl'
+#include '../utils/noise.glsl'
 
 
 void main() {
@@ -16,7 +16,7 @@ void main() {
     // that returns 1 or -1 of a random function. 
     // this will give us flipped reference frame 
     // in the x axis with 50% probability 
-    vec2 m = fract(grid * vec2( signz(hash12(floor(grid * r)) - .5) * r.x,  r.y ));
+    vec2 m = fract(grid * vec2( signz(noise(iTime*.1 + floor(grid * r)) - .5) * r.x,  r.y ));
 
     // draw a diagonal line
     float v = stroke(m.x - m.y, .1, .1, true);
