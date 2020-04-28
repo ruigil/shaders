42c30f14fe7643a7000b96529e89d4fbffbf666d
Rui Gil <rui@gil.com>
2020-04-28 19:44:29 +0000

    random parameters

--- complex/spirograph.glsl
+++ complex/spirograph.glsl
@@ -10,21 +10,21 @@
 
 void main() {
     
-    vec2 r = ref*2.5;
+    vec2 r = ref*3.+4.;
 
     int n = 5;
 
+    vec2 fl = floor(r);
+    r = fract(r)-.5;
     // the fourier serie of oscilators
     // we model a fourier series as an array of vec3
     // x = amplitude, y = frequency, z = phase;
     // by selecting the proper coefficients we can draw anything
-    vec3 fos[6] = vec3[6](
-        vec3(.2, -11.,3.),
-        vec3(.5, -7.,.0),
-        vec3(.9, -1.,2.0),
-        vec3(.4, 1.,.0),
-        vec3(.3, 7.,1.),
-        vec3(.1, 11.,.0)
+    vec3 fos[4] = vec3[4](
+        vec3(.15*hash(fl.xx), 3.,hash(fl.yy)*3.),
+        vec3(.1*hash(fl.xy), -15.,hash(fl.yx)*3.),
+        vec3(.1*hash(fl.yx), -11.,hash(fl.xy)*3.),
+        vec3(.15*hash(fl.yy), 5.,hash(fl.xx)*3.)
     );
     
     // frame feedback
@@ -37,7 +37,7 @@ void main() {
         r +=  toCarte( vec2( fos[i].x, fos[i].z + t * fos[i].y ) );
 
     // draw a point
-    f += fill(circle(r,.01), true);
+    f += fill(circle(r,.005), .01, true);
 
     oPixel = vec4(vec3(f),1.);
 }
