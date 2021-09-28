519e21a7a16f4e7152c37fba6dc7ba8237f7a0b9
Rui Gil <rui@gil.com>
2021-09-28 11:15:53 +0200

    Random imitialization

--- cellular/cyclic.glsl
+++ cellular/cyclic.glsl
@@ -12,64 +12,65 @@ uniform sampler2D u_buffer0;
 #include '../utils/2d-utils.glsl'
 #include '../utils/gradients.glsl'
 
+// The cyclic cellular automaton can be interpreted as a model to 
+// thermodynamic cycles. Each cell can be in a discrete state from 1 to N, and
+// if one of its neighbors is the successor state, modular N+1, (which means that 0 is the 
+// successor state of N, then the next iteration the cell becomes its successor.
+// We can see each state as a specie, zebras eat grass, lions eat zebras, and microbes eat lions when they die,
+// repeating the cycle.
+// https://en.wikipedia.org/wiki/Cyclic_cellular_automaton
 #if defined(BUFFER_0)
-
 float cyclic(vec2 r, float size, float ntypes) {
     vec2 current = floor(r * floor(size) ) + .5;  ; // current cell
     
-    vec2 tr = fract(unref(current/size, u_resolution));
-    float type = texture(u_buffer0, tr ).r;
+    // current type
+    float type = texture(u_buffer0, fract(unref(current/size, u_resolution)) ).r;
+    // count successors
     float cs = 0.;
 
+    // the sucessor of this type.
     float successor = mod( floor(type * (ntypes+1.)) + 1., ntypes);
+    
     // count for neighbors successors
     for(int i= -1; i <= 1; i += 1) {
         for(int j= -1; j <= 1; j += 1) {
+            // what is the neighbor type ?
             float nt = texture(u_buffer0, fract( unref((current + vec2(i,j) ) / size, u_resolution )) ).r;
-            cs +=  ( floor(nt* (ntypes+1.)) == successor) ? 1. : 0.;   
+            // if it is a successor add it to the count.
+            cs +=  float( floor(nt * (ntypes+1.)) == successor);   
         } 
     }
     
-    float s = (1./size)*.5;
-
-
-    type = cs >= 1. ? successor/ntypes : type;
-
-    //type = successor == 1. ? 1./ntypes  : type;
-
-
-
-    return type;// * fill(circle(r - (current/size), s) , EPS, true);
+    // if the cell has more than one successor, then it becomes 
+    // the successor normalized, or else it stays the same type.
+    return cs >= 1. ? (successor/ntypes) : type;
 }
+
 out vec4 pixel; 
 void main() {
 
+    // size of the simulation
     float size = 400.;
+    // number of types
     float ntypes = 24.;
-
 
     // reference frame [-0.5,0.5] with corrected aspect ratio
     vec2 rf = ref(UV, u_resolution);
-    // do the same with the mouse position
-    vec2 mouse = ref(u_mouse/u_resolution, u_resolution);
-
-    // mouse center
-    vec2 mc = vec2( rf - (floor(mouse*size)/size)) - vec2(1./(size*2.) );
-        
-
-    vec2 seed = floor(rf*size);
-    float cell = floor(hash(seed+400.)*ntypes)/ntypes;
-    //float cell = fill(circle( rf +vec2(4.*s) - s*.5, s.x*.5), EPS, true);
-        //(floor( hash( floor( (rf+100.) * SIZE) ) * NTYPES ) / NTYPES);
-        //fill(circle(rf - ((floor( rf*SIZE) + .5)/SIZE), (1./(SIZE*2.5))), EPS, true);
-    float c =  mod(floor(u_time),15.) == 0. ?  cell : cyclic(rf, size, ntypes);
+    
+    // every 15 seconds restart the simulation
+    float c =  mod(floor(u_time),15.) == 0. ? 
+        // initialized a random cell 
+        floor(hash(floor(u_time) + floor(rf*size)+400.)*ntypes)/ntypes : 
+        // perform the simulation
+        cyclic(rf, size, ntypes);
 
     pixel = vec4(vec3(c),1.); 
 }
 #else
-// we just copy the texture buffer to the screen...
+// we just copy the texture buffer to the screen
+// and apply a gradient to the types...
 out vec4 pixel; 
 void main() {
-    pixel = vec4( lava(texture(u_buffer0,UV).r),1.);
+    pixel = vec4( lava( texture(u_buffer0,UV).r) , 1.);
 }
 #endif  
