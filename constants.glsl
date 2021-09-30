027574a7a5490265c9d9af752cab3aaf1db84c79
Rui Gil <rui@gil.com>
2021-09-30 21:43:48 +0200

    define XY gl_FragCoord

--- constants.glsl
+++ constants.glsl
@@ -1,6 +1,11 @@
-#define UV (gl_FragCoord.xy/u_resolution.xy)
-#define PIXEL_SIZE (1./u_resolution)
-#define EPS (1./ (u_resolution.x < u_resolution.y ? u_resolution.x : u_resolution.y ))
+#define T u_time
+#define M u_mouse
+#define R u_resolution
+
+#define XY gl_FragCoord.xy
+#define UV (XY / R)
+#define PIXEL_SIZE (1. / R)
+#define EPS ( 1./ (R.x < R.y ? R.x : R.y ) )
 
 #define PI 3.1415926536
 #define TAU 6.2831853072
