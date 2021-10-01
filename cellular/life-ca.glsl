2cfe7e6f0af13b9be1f9f3592d35b346ae514ba0
Rui Gil <rui@gil.com>
2021-10-01 15:18:27 +0200

    Refactor coordinate system

--- cellular/life-ca.glsl
+++ cellular/life-ca.glsl
@@ -1,17 +1,15 @@
 #version 300 es
 precision highp float;
 
-uniform vec2 u_resolution;
-uniform vec2 u_mouse;
-uniform float u_time;
+uniform vec2 u_resolution; // R
+uniform vec2 u_mouse; // M
+uniform float u_time; // T
 // buffer to keep the state of the simulation
 uniform sampler2D u_buffer0;
 
 #include '../constants.glsl'
 #include '../utils/noise.glsl'
 #include '../utils/2d-utils.glsl'
-
-#define SIZE 70.
 
 // Conway Game of Life 
 // https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
@@ -20,54 +18,57 @@ uniform sampler2D u_buffer0;
 // that we will use to keep the state of the simulation
 #if defined(BUFFER_0)
 
-float life(vec2 r) {
-    vec2 current = floor( r*SIZE) + .5; // current cell
-
+float life(sampler2D t, vec2 p, float scale) {
     // we read the previous state from the texture buffer, to see if the cell is alive... 
-    float alive = 1.-texture(u_buffer0, fract(unref( (current/SIZE), u_resolution )) ).r; 
-    float nb = 0.; // neighbours
+    float alive = 1. - texture( t, p ).r; 
+    float nb = 0.; // neighbors
 
-    // we count the neighbours...
-    for(int i= -1; i <= 1; i += 1) {
-        for(int j= -1; j <= 1; j += 1) {
-            nb += 1.-texture(u_buffer0, fract( unref((current + vec2(i,j) ) / SIZE, u_resolution )) ).r;
-        }
-    }
+    // count the number of alive neightbors of this pixel (8+1) 
+    for(int i=0; i<9; i++) {
+        vec2 offset = ( vec2( (i/3) - 1 , (i%3) - 1 ) * PIXEL_SIZE * scale  );
+        nb += 1. - texture(t, fract(p + offset) ).r;
+    } 
+
     nb -= alive; // do not count self
     
-    // paint a little cicle for the cells that are alive
-    float cell = fill(circle(r - (current/SIZE), (1./(SIZE*2.5))), EPS, true);
     // rules... 
     // 1) if alive and 2 or 3 neighbors stay alive
     // 2) if dead and exactly three neighbors then born
     // 3) else dead...
-    return (bool(alive) && (nb == 2. || nb == 3.)) || (!bool(alive) && nb == 3.) ? cell : 0.;
+    return float( (bool(alive) && (nb == 2. || nb == 3.)) || (!bool(alive) && nb == 3.) );
 }
 out vec4 pixel;
 void main() {
+    // the scale of the simulation (size of the pixels)
+    float scale = 10.;
+    
+    // XY is the coodinate of the pixel gl_FragCoord.xy
+    // but because coordinates are for the middle of the pixel, 
+    // they are 1.5, 2.5, 3.5,... we need to subtract .5 and
+    // add it in the simulation with the correct scale. 
+    vec2 p = (( (floor( (XY - .5) / scale ) * scale ) ) / R) ;
 
-    // reference frame [-0.5,0.5] with corrected aspect ratio
-    vec2 rf = ref(UV, u_resolution);
-    // do the same with the mouse position
-    vec2 mouse = ref(u_mouse/u_resolution, u_resolution);
-
-    // mouse center
-    vec2 mc = vec2( rf - (floor(mouse*SIZE)/SIZE)) - vec2(1./(SIZE*2.) );
+    // normalize the mouse position for the pixel center
+    vec2 mouse = M / R;
 
     float l =
         // random cell initialisation 
-        floor( hash( u_time + floor(rf*SIZE) ) + .5 ) *
+        floor( hash( T + p ) + .5 ) *
         // in a 3x3 square size at the mouse center 
-        fill( rect( mc, vec2(3./SIZE) ), EPS, true ) +
+        fill(rect( p - mouse, PIXEL_SIZE * scale * 3. ), EPS, true) +
         // plus life simulation 
-        life(rf);
+        life(u_buffer0, p + (PIXEL_SIZE * scale *.5) /* add half pixel */, scale);
 
+    // paint a little cicle for the cells that are alive
+    vec2 r = ( (UV - p) / ( PIXEL_SIZE * scale) ) - .5;
+    l *= fill(circle(r,.5), EPS, true);
+    
     pixel = vec4(vec3(1.-l),1.); 
 }
 #else
 // we just copy the texture buffer to the screen...
 out vec4 pixel;
 void main() {
-    pixel = vec4(texture(u_buffer0,UV).rgb,1.);
+    pixel = vec4(texture(u_buffer0 , UV).rgb,1.);
 }
 #endif
